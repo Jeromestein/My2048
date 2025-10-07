474f006b94347af2f254f2f4ab64ed1c1a6ed890
Jerome <nietsemorej@gmail.com>
2025-10-07 16:13:36 -0700

    Initial Commit

---
+++ My2048 macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  My2048 macOS
+//
+//  Created by Jiayi on 10/7/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ My2048 macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  My2048 macOS
+//
+//  Created by Jiayi on 10/7/25.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
